659ce32d6d62c5985f307ef4a44a0efdc15e8bbf
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2022-03-22 19:13:06 +0100

    array

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-
+#![recursion_limit = "256"]
 use serde_json::Value;
 use std::fs;
 
 macro_rules! json_extract {
-    ($keys:expr,$json:expr,$t:ty, $($counter:expr)?) => {{
+    ($keys:expr,$json:expr,$t:ty,$nt:ty, $($counter:expr)?) => {{
         let chain: Vec<&str> = $keys.split(".").collect();
-        let mut res: Option<$t> = None;
-        
         let mut prev_key: Option<&Value> = None;
         let mut counter: usize = 0;
+        let mut res: Option<$t> = None;
+        let how_many_arrays = chain.iter().filter(|&n| *n == "$").count();
+
+        if how_many_arrays == 1 {
+            
+        }
         // next feature
         $(
-            if $counter> 0 {counter = $counter};
+            if $counter> 0 {
+                counter = $counter
+            };
+            
+            
         )*
 
         fn get_value<'a>(mut _prev_key: Option<&'a Value>, _key: &'a str) -> Option<&'a Value> {
@@ -25,39 +33,57 @@ macro_rules! json_extract {
 
         fn get_final_value<'a>(mut _prev_key: Option<&'a Value>, _key: &'a str) -> Option<$t> {
             if let Value::Object(val_return) = _prev_key.unwrap() {
-                let val_return = val_return.get(_key).unwrap();
+                let val_return: &Value = val_return.get(_key).unwrap();
                 serde_json::from_value(val_return.clone()).unwrap_or_else(|_| None)
             } else {
                 None
             }
         }
 
+        fn get_final_value_array<'a>(mut _prev_key: Option<&'a Value>, _key: &'a str) -> Option<$nt> {
+            if let Value::Object(val_return) = _prev_key.unwrap() {
+                let val_return: &Value = val_return.get(_key).unwrap();
+                let r = serde_json::from_value(val_return.clone());
+                Some(r.unwrap_or_default())
+
+            } else {
+                None
+            }
+        }
+        
         while counter < chain.len() {
+
             let key: &str = chain[counter];
             if counter == chain.len() - 1 {
                 res = get_final_value(prev_key, key);
                 break;
             }
-            // if key == "$" {
-            //     if let Value::Array(val_return) = prev_key.unwrap() {
-            //         let vals = vec::New();
-            //         for _ in val_return {
-            //             let mut sliced_chain = Vec::new();
-            //             for _ in 0..counter{
-            //                  sliced_chain.push("#");
-            //             }
-            //             let sliced_chain = sliced_chain.join(".");
-            //             let _sliced_chain = &chain[&counter + 1..].join(".");
-            //             let sliced_chain = format!("{}.{}",sliced_chain,_sliced_chain);
-            //             println!("x - {:?}", sliced_chain);
+            if key == "$" {
+                if let Value::Array(val_return) = prev_key.unwrap() {
+                    let mut vals = Vec::new();
+                    for current_val in val_return {
+                        let mut sliced_chain = Vec::new();
+                        for _ in 0..counter{
+                             sliced_chain.push("#");
+                        }
                         
-            //             let x = json_extract!(&sliced_chain, $json.clone(), $t, counter.clone() + 1);
-            //             vals.push(x);
-            //         }
-            //         break;
-                    
-            //     }
-            // }
+                        if counter+1 == chain.len()-1  {
+                            let val = get_final_value_array(Some(current_val), &chain[&counter + 1..][0]);
+                            
+                            vals.push(val.unwrap());
+                        }
+                        // else{
+                            // let sliced_chain = sliced_chain.join(".");
+                            // let _sliced_chain = &chain[&counter + 1..].join(".");
+                            // let sliced_chain = format!("{}.{}",sliced_chain,_sliced_chain);
+
+                        //     json_extract!(sliced_chain, $json.clone(), $t,$nt,counter)
+                        // }
+                    }
+                    res = Some(vals);
+                    break;
+                }
+            }
             if prev_key.is_none() {
                 prev_key = get_value(Some($json), key);
             } else {
@@ -65,16 +91,16 @@ macro_rules! json_extract {
             }
             counter += 1;
         }
+      
         res
     }};
 }
-
 
 fn main() {
     let path = "./src/test.json";
     let data = fs::read_to_string(path).unwrap();
     let res: Value = serde_json::from_str(&data).unwrap();
-    let x = json_extract!("brand.tesla.models.designers", &res, Vec<String>,);
-    
+    let x = json_extract!("brand.tesla.models.$.designers", &res, Vec<Vec<String>> ,Vec<String>,);
+
     println!(" $ {:?}", x.unwrap_or_default());
 }
