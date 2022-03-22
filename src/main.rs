ff49aca69e22f9b65a131d929d01eb645c861c65
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2022-03-23 00:24:14 +0100

    c

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-<<<<<<< Updated upstream
-=======
 #![recursion_limit = "256"]
 use serde::de::DeserializeOwned;
->>>>>>> Stashed changes
 use serde_json::Value;
 use std::fs;
 
 macro_rules! json_extract {
     ($keys:expr,$json:expr,$t:ty, $($counter:expr)?) => {{
-<<<<<<< Updated upstream
-        let chain: Vec<&str> = $keys.split(".").collect();
-        let mut res: Option<$t> = None;
-        let mut prev_key: Option<&Value> = None;
-        let mut counter: usize = 0;
-
-        $(
-            if $counter> 0 {counter = $counter};
-=======
 
         let chain: Vec<&str> = $keys.split(".").collect();
         let prev_key: Option<&Value> = None;
@@ -35,7 +23,6 @@ macro_rules! json_extract {
                 counter = $counter
             };
            
->>>>>>> Stashed changes
         )*
 
         fn get_value<'a>(mut _prev_key: Option<&'a Value>, _key: &'a str) -> Option<&'a Value> {
@@ -51,10 +38,6 @@ macro_rules! json_extract {
             where T: DeserializeOwned + std::fmt::Debug
         {
             if let Value::Object(val_return) = _prev_key.unwrap() {
-<<<<<<< Updated upstream
-                let val_return = val_return.get(_key).unwrap();
-                serde_json::from_value(val_return.clone()).unwrap_or_else(|_| None)
-=======
                 
                 let val_return: Value = val_return.get(_key).unwrap().clone();
                 println!("val_return - {:?}",val_return);
@@ -62,30 +45,11 @@ macro_rules! json_extract {
                 println!("t - {:?}",&s.as_ref().unwrap());
 
                 Some(s.unwrap())
->>>>>>> Stashed changes
             } else {
                 None
             }
         }
 
-<<<<<<< Updated upstream
-        while counter < chain.len() {
-            let key: &str = chain[counter];
-            if counter == chain.len() - 1 {
-                res = get_final_value(prev_key, key);
-                break;
-            }
-
-            if prev_key.is_none() {
-                prev_key = get_value(Some($json), key);
-            } else {
-                prev_key = get_value(prev_key, key);
-            }
-            counter += 1;
-        }
-        res
-    }};
-=======
 
         fn json_loop<'a,T>(mut counter: usize, chain: Vec<&'a str>,mut prev_key:Option<&'a Value>, json: &'a Value ) -> Option<T>
             where T: DeserializeOwned + std::fmt::Debug
@@ -95,7 +59,7 @@ macro_rules! json_extract {
 
                 let key: &str = chain[counter];
                 if counter == chain.len() - 1 {
-                    // r = get_final_value::<T>(prev_key, key);
+                    r = get_final_value::<T>(prev_key, key);
                     break;
                 }
                 if key == "$" {
@@ -121,7 +85,7 @@ macro_rules! json_extract {
                             //     json_extract!(sliced_chain, $json.clone(), $t,$nt,counter)
                             // }
                         }
-                        r = Some(vals);
+                        // r = Some(vals);
                         println!(" vals - {:?}",vals);
                         break;
                     }
@@ -142,22 +106,16 @@ macro_rules! json_extract {
         x
     }
 };
->>>>>>> Stashed changes
 }
 
 fn main() {
     let path = "./src/test.json";
     let data = fs::read_to_string(path).unwrap();
-<<<<<<< Updated upstream
-    let res: Value = serde_json::from_str(&data).unwrap();
-    let x = json_extract!("brand.tesla.model.designers", &res, Vec<String>,);
-=======
     let json_parsed: Value = serde_json::from_str(&data).unwrap();
   
 
 
     let result = json_extract!("brand.tesla.models.$.designers", &json_parsed, String,);
->>>>>>> Stashed changes
 
     println!(" $ {:?}", result.unwrap_or_default());
-}
+
